c4ff1d7cf6591d80f600b2877ba462e49e35c331
yehtet <yhtet1934@gmail.com>
2026-01-09 17:46:13 +0800

    Implemnet posthog

--- app/components/ExploreBtn.tsx
+++ app/components/ExploreBtn.tsx
@@ -1,13 +1,22 @@
+'use client'
+
 import Image from "next/image"
+import posthog from "posthog-js"
 
 const ExploreBtn = () => {
   return (
-    <button type="button" id="explore-btn" className="mt-7 mx-auto">
-      <a href="#events">
-        Explore Events
-        <Image src="/icons/arrow-down.svg" alt="arrow-down" width="24" height="24" />
-      </a>
-    </button>
+    <div>
+      <button type="button" id="explore-btn" className="mt-7 mx-auto">
+        <a href="#events">
+          Explore Events
+          <Image src="/icons/arrow-down.svg" alt="arrow-down" width="24" height="24" />
+        </a>
+      </button>
+
+      <button onClick={() => posthog.capture('test_event')}>
+        Click me for an event
+      </button>
+    </div>
   )
 }
 
